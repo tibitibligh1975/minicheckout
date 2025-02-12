6f8719ef7c7ddf16a08caf30e8eb17f5a9449d63
PokerDoDigital <pokerdodigital@gmail.com>
2025-02-12 00:42:49 -0300

    feat: add home page with MiniCheckout component

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import MiniCheckout from "@/components/MiniCheckout";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <MiniCheckout />
+    </main>
+  );
+} 
